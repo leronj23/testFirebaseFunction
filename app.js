e40db67f5347e09dc66d445cfbcf780aba4be3f9
LeRon <leronj23@gmail.com>
2019-01-18 09:18:16 -0500

    update

--- app.js
+++ app.js
@@ -11,23 +11,26 @@ var config = {
 firebase.initializeApp(config);
 
 // Used to test Cloud Functions Locally. Get localhost number afer firebase serve --only functions
-// const app = firebase.initializeApp(config);
-// app.functions().useFunctionsEmulator('http://localhost:5000');
+//const app = firebase.initializeApp(config);
+//app.functions().useFunctionsEmulator('http://localhost:5000');
 
 // Set Firebase Database
 var database = firebase.database();
 
 var driverId = "Its working!";
 
-var getVehicleById = firebase.functions().httpsCallable('getVehicleById');
+//var getVehicleById = firebase.functions().httpsCallable('getVehicleById');
+var getToken = firebase.functions().httpsCallable('getToken');
+
 
 // When you want to send arguments to CF.
-getVehicleById({ text: driverId }).then(function (result) {
+//getVehicleById({ text: driverId }).then(function (result) {
+getToken().then(function (result) {
 
-// When you dont want to send arguments to CF. Use '()'
-//message().then(function (result) {
+    // When you dont want to send arguments to CF. Use '()'
+    //message().then(function (result) {
 
-    console.log('objectSent', result.data)
+    console.log('objectSent', result)
 
 }).catch(function (error) {
 
